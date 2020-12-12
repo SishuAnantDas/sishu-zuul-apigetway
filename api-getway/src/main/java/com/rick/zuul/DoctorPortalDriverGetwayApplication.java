8d1cd28de10272ec1df2005e40a95fbfc9d05a8e
sishu <sishu@DESKTOP-VA5560V>
2020-12-12 19:42:59 +0530

    zuul-api-getway project creation

---
+++ api-getway/src/main/java/com/rick/zuul/DoctorPortalDriverGetwayApplication.java
@@ -0,0 +1,13 @@
+package com.rick.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorPortalDriverGetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorPortalDriverGetwayApplication.class, args);
+	}
+
+}
