a2a9271dff6ad9ae78c7af11e84b5cc2c95acf88
sishu <sishu@DESKTOP-VA5560V>
2020-12-13 11:59:42 +0530

    patient-service creation

---
+++ patient-service/src/main/java/com/rick/zuul/PatientDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.rick.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientDetailsApplication.class, args);
+	}
+
+}
