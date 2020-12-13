2199a3ca9afa550fdd90fcb7a74c3093ad6c5f2b
sishu <sishu@DESKTOP-VA5560V>
2020-12-13 11:57:30 +0530

    doctor-service creation

---
+++ doctor-service/src/main/java/com/rick/zuul/DoctorDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.rick.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorDetailsApplication.class, args);
+	}
+
+}
